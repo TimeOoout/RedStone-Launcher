6cab5a970e2b668b19994cca4d80f944b1a52745
TimeOut <TimeOoout@outlook.com>
2022-10-23 01:01:33 +0800

    添加初始化核心

--- RSL_Core/RSL_Log/Log_Printer.go
+++ RSL_Core/RSL_Log/Log_Printer.go
@@ -1,22 +1,21 @@
 package RSL_Log
 
 import (
-	"fmt"
 	"github.com/fatih/color"
 )
 
 func PrintInfo(format string, args ...interface{}) {
-	fmt.Printf(color.CyanString("[INFO] "+format+"\n", args...))
+	color.Cyan("[INFO] "+format+"\n", args...)
 }
 
 func PrintWarning(format string, args ...interface{}) {
-	fmt.Printf(color.YellowString("[Warning] "+format+"\n", args...))
+	color.Yellow("[Warning] "+format+"\n", args...)
 }
 
 func PrintError(format string, args ...interface{}) {
-	fmt.Printf(color.RedString("[Error] "+format+"\n", args...))
+	color.Red("[Error] "+format+"\n", args...)
 }
 
 func PrintDebug(format string, args ...interface{}) {
-	fmt.Printf(color.GreenString("[Debug] "+format+"\n", args...))
+	color.Green("[Debug] "+format+"\n", args...)
 }
